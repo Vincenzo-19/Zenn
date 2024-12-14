a15b63905914041b09cfab979d5e5c5f9565ee42
VincenzoNurcato <nurcatov@gmail.com>
2024-12-14 14:05:58 +0100

    effettuato modifica layout h3 footer

--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -3,7 +3,9 @@ export const Footer = () => {
         <footer className="bg-primaryColor text-white py-8">
             <div className="container mx-auto flex flex-col-reverse sm:flex-row justify-between items-center">
                 <div className="sm:ml-8">
-                    <h3 className="text-2xl font-bold mb-4">Meditation App</h3>
+                    <h3 className="text-center sm:text-start text-2xl font-bold mb-4">
+                        Zenn
+                    </h3>
                     <p className="text-sm">© 2024 Zenn. All rights reserved.</p>
                 </div>
                 <div className="flex gap-8 mb-8 sm:mb-0 sm:mr-8">
