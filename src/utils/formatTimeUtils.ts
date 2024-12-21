902523f058b11772cf08e7f2328e01aa61819302
VincenzoNurcato <nurcatov@gmail.com>
2024-12-21 16:47:42 +0100

    aggiunto lunox per formattare la durata

--- src/utils/formatTimeUtils.ts
+++ src/utils/formatTimeUtils.ts
@@ -1,10 +1,8 @@
+import { Duration } from 'luxon';
+
 // funzione per formattare il tempo in minuti e secondi
 
 export const formatTime = (time: number): string => {
-    const MINUTES = Math.floor(time / 60);
-    const SECONDS = time % 60;
-    return `${String(MINUTES).padStart(2, '0')}:${String(SECONDS).padStart(
-        2,
-        '0'
-    )}`;
+    const DURATION = Duration.fromObject({ seconds: time });
+    return DURATION.toFormat('mm:ss');
 };
